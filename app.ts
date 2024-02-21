127bc0e76749bb150319d92a907edc7ce991194d
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-02-21 16:02:21 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,3 @@
+let qoute: string= "A person who never made a mistake never made a mistake never tried anything now";
+let author: string= "Albert Einstein"
+console.log(`${author} once said, "${qoute}`);
